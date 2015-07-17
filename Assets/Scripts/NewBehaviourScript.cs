d84f7b04fb591e448889ffe48af684437a9aaef1
Alexander Helbig <alexander.b.helbig@stud.h-da.de>
2015-07-17 20:20:48 +0200

    Slingshooter_Final_commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	private GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	/*void Update() {
+		
+		if (projectile.transform.position.y <= -10&&projectile != null) {
+			projectile.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		}
+}*/
+}
